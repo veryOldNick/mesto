027186d6eb8e6e0d5196fbb11275851de2e43348
Nikolai Krasa <greyrat1@yandex.ru>
2023-03-23 17:40:20 +0300

    ПР6 first iteration

--- scripts/validate.js
+++ scripts/validate.js
@@ -2,86 +2,75 @@ const options = {
   formSelector: '.popup__form',
   inputSelector: '.popup__input',
   submitButtonSelector: '.popup__button-save',
-  inactiveButtonClass: 'popup__button_submit_disabled',
+  inactiveButtonClass: 'popup__button_save_disabled',
   inputErrorClass: 'popup__input_type_error',
   errorClass: 'popup__input-error_visible'
 };
-//Функция показывает сообщение об ошибке
-const showInputError = (formElement, inputElement, errorMessage, { inputErrorClass, errorClass }) => {
-  inputElement.classList.add(inputErrorClass);
-  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
-  errorElement.textContent = errorMessage;
-  errorElement.classList.add(errorClass);
+
+
+//Функция выводит сообщение об ошибке
+const showInputError = (formElement, inputElement, options) => {
+  const errorElement = formElement.querySelector(`.${inputElement.name}-error`);
+    
+  inputElement.classList.add(options.inputErrorClass);
+  errorElement.textContent = inputElement.validationMessage;
+  errorElement.classList.add(options.errorClass);
 };
-//Функция прячет сообщение об ошибке
-const hideInputError = (formElement, inputElement, { inputErrorClass, errorClass }) => {
-  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
-  inputElement.classList.remove(inputErrorClass);
-  errorElement.classList.remove(errorClass);
+
+//Функция скрывает сообщение об ошибке
+const hideInputError = (formElement, inputElement, options) => {
+  const errorElement = formElement.querySelector(`.${inputElement.name}-error`);
+    
+  inputElement.classList.remove(options.inputErrorClass);
+  errorElement.classList.remove(options.errorClass);
   errorElement.textContent = '';
 };
-//Скрываем сообщения об ошибке при открытие
-const setDefaultErrorState = (formElement) => {
-  const inputList = Array.from(formElement.querySelectorAll(options.inputSelector));
-  inputList.forEach((inputElement) => {
+
+// Функция показывает/скрывает ошибку у инпута
+const checkInputValidity = (formElement, inputElement, options) => {
+  
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, options);
+  } else {
     hideInputError(formElement, inputElement, options);
-  });
+  }
 };
-// проверяем есть ли инпуты не прошедшие валидацию
+
+// Функция проверяет есть ли инпуты не прошедшие валидацию
 const hasInvalidInput = (inputList) => {
   return inputList.some((inputElement) => {
     return (!inputElement.validity.valid || !inputElement.value);
   });
 };
-// Функция показывает/скрывает ошибку у инпута
-const checkInputValidity = (formElement, inputElement, options) => {
-  if (!inputElement.validity.valid) {
-    showInputError(formElement, inputElement, inputElement.validationMessage, options);
-  } else {
-    hideInputError(formElement, inputElement, options);
-  }
-};
-//Дефолтное состояние кнопки при открытие
-const setDefaultButtonState = (buttonElement, inactiveButtonClass) => {
-  buttonElement.classList.add(inactiveButtonClass);
-};
+
 //Функция делает кнопку активной/неактивной, если инпуты прошли/не прошли валидацию
-const toggleButtonState = (inputList, buttonElement, { inactiveButtonClass }) => {
+const toggleButtonState = (inputList, buttonElement, options) => {
   if (hasInvalidInput(inputList)) {
-    buttonElement.classList.add(inactiveButtonClass);
+    buttonElement.classList.add(options.inactiveButtonClass);
   } else {
-    buttonElement.classList.remove(inactiveButtonClass);
+    buttonElement.classList.remove(options.inactiveButtonClass);
   }
 };
-//Функция вызывает обработчики событий для инпутов, кнопок
-const setEventListeners = (formElement, { inputSelector, submitButtonSelector, ...options }) => {
-  const inputList = Array.from(formElement.querySelectorAll(inputSelector));
-  const buttonElement = formElement.querySelector(submitButtonSelector);
+
+/*Функция перебирает инпуты, вызывает обработчики */
+const setEventListeners = (formElement, options) => {
+  const inputList = Array.from(formElement.querySelectorAll(options.inputSelector));
+  const buttonElement = formElement.querySelector(options.submitButtonSelector);
+  
   toggleButtonState(inputList, buttonElement, options);
-  inputList.forEach((inputElement) => {
-    inputElement.addEventListener('input', () => {
+  inputList.forEach((inputElement) => {    
+    inputElement.addEventListener('input', () => {      
       checkInputValidity(formElement, inputElement, options);
       toggleButtonState(inputList, buttonElement, options);
     });
   });
 };
-//Сама функция валидации
-const enableValidation = ({ formSelector, ...options }) => {
-  const formList = Array.from(document.querySelectorAll(formSelector));
-  
-  formList.forEach((formElement) => {
-    formElement.addEventListener('submit', (evt) => {
-      evt.preventDefault();
-      const buttonElement = formElement.querySelector(options.submitButtonSelector);
 
-      console.log('test');
-      console.log(buttonElement);
+/*Функция перебирает формы */
+const enableValidation = (options) => {
+  const formList = Array.from(document.querySelectorAll(options.formSelector));
 
-      setDefaultButtonState(buttonElement, options.inactiveButtonClass);
-      evt.preventDefault();
-    });
-    setEventListeners(formElement, options);
-  });
+  formList.forEach((formElement) => {setEventListeners(formElement, options);});
 };
-//Вызов функции enableValidation и передача ей объекта, как аргумента
+
 enableValidation(options);
